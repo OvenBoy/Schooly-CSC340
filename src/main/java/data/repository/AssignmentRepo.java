327c85f499d3eedb297a298dab0b6ac1c0397708
EvanCorsar <111367216+EvanCorsar@users.noreply.github.com>
2022-10-20 15:01:01 -0400

    Delete src directory

--- src/main/java/data/repository/AssignmentRepo.java
+++
@@ -1,8 +0,0 @@
-package data.repository;
-
-import data.Stu_Assignments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepo extends JpaRepository<Stu_Assignments, String> {
-
-}
--- src/main/java/data/repository/InstructorRepository.java
+++
@@ -1,6 +0,0 @@
-package data.repository;
-import data.entity.Instructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-}
